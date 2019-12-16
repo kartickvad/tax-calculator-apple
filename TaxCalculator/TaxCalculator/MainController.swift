b3871f0590d5cc84c3391cd03f2341cc736a33c1
Ajo M Varghese <ajo@jupitersupplystation.com>
2019-12-16 14:08:03 +0530

    Add outlets and actions for the textfields

--- TaxCalculator/TaxCalculator/MainController.swift
+++ TaxCalculator/TaxCalculator/MainController.swift
@@ -10,9 +10,54 @@ import UIKit
 
 class MainController: UIViewController {
 
+  // MARK:- Lifecycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
+  }
+
+
+
+
+  // MARK:- Outlets
+
+
+  @IBOutlet weak var takeHomePayTextfield: UITextField!
+  @IBOutlet weak var ctcForEmployeeTextField: UITextField!
+  @IBOutlet weak var professionalTaxTextfield: UITextField!
+  @IBOutlet weak var pfRateTextField: UITextField!
+  @IBOutlet weak var presumtiveTaxationRateTextField: UITextField!
+  @IBOutlet weak var gstRateTextField: UITextField!
+
+
+
+
+  // MARK:- IBActions
+
+  @IBAction func takeHomePayChanged(_ sender: UITextField) {
+    let userEnterdText = Int(takeHomePayTextfield.text!)
+    // TODO: Calculate
+  }
+
+  @IBAction func ctcForEmployeeChanged(_ sender: UITextField) {
+    // TODO: Calculate
+  }
+
+  @IBAction func professionalTaxValueChanged(_ sender: UITextField) {
+    // TODO: Calculate
+  }
+
+  @IBAction func pfRateChanged(_ sender: UITextField) {
+    // TODO: Calculate
+  }
+
+  @IBAction func presumtiveTaxationRateChanged(_ sender: UITextField) {
+    // TODO: Calculate
+  }
+
+  @IBAction func gstRateChanged(_ sender: UITextField) {
+    // TODO: Calculate
   }
 }
 
