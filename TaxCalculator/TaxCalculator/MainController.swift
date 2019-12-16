8d4d2e021d8ef70122135d0a62afbc476602958d
Ajo M Varghese <ajo@jupitersupplystation.com>
2019-12-16 13:33:22 +0530

    Add MainController as the only controller

--- TaxCalculator/TaxCalculator/MainController.swift
+++ TaxCalculator/TaxCalculator/MainController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainController.swift
 //  TaxCalculator
 //
 //  Created by Akshit Talwar on 16/12/19.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
   }
-
-
 }
 
